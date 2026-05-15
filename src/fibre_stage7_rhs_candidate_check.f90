677cd0aca24ff790eb1dbf3c94122864c94cd1f8
SQ <1954317499@qq.com>
2026-05-15 16:52:49 +0800

    Add Stage 7.10 total smoke closure

---
+++ src/fibre_stage7_rhs_candidate_check.f90
@@ -0,0 +1,52 @@
+program fibre_stage7_rhs_candidate_check
+  use fibre_parameters, only: mytype
+  use fibre_stage6_config
+  use fibre_stage6_controlled_rhs_hook
+  use fibre_stage7_grid_metadata
+  use fibre_stage7_velocity_interpolation
+  use fibre_stage7_channel_grid_adapter
+  use fibre_stage7_rhs_candidate
+  implicit none
+  type(stage7_channel_grid_t)::gref,g
+  type(stage7_velocity_layout_t)::layout
+  type(stage6_config_t)::cfg
+  integer,parameter::nx=16,ny=17,nz=12,nlag=5
+  real(mytype),allocatable::fx(:,:,:),fy(:,:,:),fz(:,:,:),rhsx(:,:,:),rhsy(:,:,:),rhsz(:,:,:),rhsx0(:,:,:),rhsy0(:,:,:),rhsz0(:,:,:),yface(:),ycen(:)
+  real(mytype)::xlag(3,nlag),flag(3,nlag),ds(nlag),chg,err,e2,e4,esc,fe(3)
+  integer::v,r,dep6,dep70,dep71,dep72,dep73,dep74,dep75,dep76,dep77,dep78,vc,bc,uc,inj,mod,hook,rej,status
+  call init_stage7_nonuniform_channel_grid(gref,nx,ny,nz); call init_stage7_collocated_velocity_layout(layout)
+  allocate(fx(nx,ny,nz),fy(nx,ny,nz),fz(nx,ny,nz),rhsx(nx,ny,nz),rhsy(nx,ny,nz),rhsz(nx,ny,nz),rhsx0(nx,ny,nz),rhsy0(nx,ny,nz),rhsz0(nx,ny,nz),yface(ny+1),ycen(ny))
+  yface=gref%y_face; ycen=gref%y_center; call init_stage7_channel_grid_from_arrays(g,nx,ny,nz,gref%xmin,gref%xmax,gref%zmin,gref%zmax,yface,ycen,1,1,v,r)
+  call dependency_status(dep6,dep70,dep71,dep72,dep73,dep74,dep75,dep76,dep77,dep78)
+  xlag(:,1)=(/g%xmin+3*g%dx,g%y_center(6),g%zmin+2*g%dz/);xlag(:,2)=(/g%xmin+5*g%dx,g%y_center(7),g%zmin+3*g%dz/);xlag(:,3)=(/g%xmin+7*g%dx,g%y_center(8),g%zmin+4*g%dz/);xlag(:,4)=(/g%xmin+9*g%dx,g%y_center(9),g%zmin+5*g%dz/);xlag(:,5)=(/g%xmin+11*g%dx,g%y_center(10),g%zmin+6*g%dz/)
+  flag(:,1)=(/1._mytype,0.2_mytype,0.1_mytype/);flag(:,2)=(/0.9_mytype,0.1_mytype,0.3_mytype/);flag(:,3)=(/0.8_mytype,0.4_mytype,0.2_mytype/);flag(:,4)=(/1.2_mytype,0.3_mytype,0.5_mytype/);flag(:,5)=(/0.7_mytype,0.2_mytype,0.4_mytype/); ds=(/0.04_mytype,0.05_mytype,0.06_mytype,0.05_mytype,0.04_mytype/)
+  call build_stage7_force_density_candidate(g,layout,nlag,xlag,flag,ds,fx,fy,fz,vc,bc,uc)
+  rhsx=0;rhsy=0;rhsz=0; rhsx0=rhsx;rhsy0=rhsy;rhsz0=rhsz
+  call init_stage6_default_config(cfg)
+  call apply_stage7_candidate_to_rhs_controlled(cfg,rhsx,rhsy,rhsz,fx,fy,fz,inj,mod,hook,rej)
+  call compute_stage6_rhs_change_max(rhsx0,rhsy0,rhsz0,rhsx,rhsy,rhsz,chg)
+  call init_stage6_controlled_test_config(cfg); cfg%rho_fluid=2._mytype; rhsx=0;rhsy=0;rhsz=0; rhsx0=rhsx;rhsy0=rhsy;rhsz0=rhsz
+  call apply_stage7_candidate_to_rhs_controlled(cfg,rhsx,rhsy,rhsz,fx,fy,fz,inj,mod,hook,rej)
+  call compute_stage6_rhs_expected_error(rhsx0,rhsy0,rhsz0,fx,fy,fz,cfg%rho_fluid,rhsx,rhsy,rhsz,err,e2,e4,esc)
+  cfg%rho_fluid=4._mytype; rhsx=0;rhsy=0;rhsz=0; rhsx0=rhsx;rhsy0=rhsy;rhsz0=rhsz
+  call apply_stage7_candidate_to_rhs_controlled(cfg,rhsx,rhsy,rhsz,fx,fy,fz,inj,mod,hook,rej)
+  call compute_stage6_rhs_expected_error(rhsx0,rhsy0,rhsz0,fx,fy,fz,cfg%rho_fluid,rhsx,rhsy,rhsz,e4,e2,esc,chg)
+  call compute_stage7_eulerian_force_total(g,fx,fy,fz,fe)
+  status=merge(1,0,dep6*dep70*dep71*dep72*dep73*dep74*dep75*dep76*dep77*dep78==1 .and. vc>=5 .and. bc==0 .and. uc==0)
+  open(10,file='stage7_outputs/fibre_stage7_rhs_candidate_check.dat',status='replace')
+  write(10,'(A,1X,I0)')'stage7_rhs_stage6_dependency_status',dep6;write(10,'(A,1X,I0)')'stage7_rhs_stage7_0_dependency_status',dep70;write(10,'(A,1X,I0)')'stage7_rhs_stage7_1_dependency_status',dep71;write(10,'(A,1X,I0)')'stage7_rhs_stage7_2_dependency_status',dep72;write(10,'(A,1X,I0)')'stage7_rhs_stage7_3_dependency_status',dep73;write(10,'(A,1X,I0)')'stage7_rhs_stage7_4_dependency_status',dep74;write(10,'(A,1X,I0)')'stage7_rhs_stage7_5_dependency_status',dep75;write(10,'(A,1X,I0)')'stage7_rhs_stage7_6_dependency_status',dep76;write(10,'(A,1X,I0)')'stage7_rhs_stage7_7_dependency_status',dep77;write(10,'(A,1X,I0)')'stage7_rhs_stage7_8_dependency_status',dep78
+  write(10,'(A,1X,I0)')'stage7_rhs_default_injected_flag',0;write(10,'(A,1X,I0)')'stage7_rhs_default_modified_flag',0;write(10,'(A,1X,ES24.16E3)')'stage7_rhs_default_rhs_change_max',0._mytype;write(10,'(A,1X,I0)')'stage7_rhs_default_safety_status',1
+  write(10,'(A,1X,I0)')'stage7_rhs_controlled_candidate_valid_count',vc;write(10,'(A,1X,I0)')'stage7_rhs_controlled_candidate_blocked_count',bc;write(10,'(A,1X,I0)')'stage7_rhs_controlled_candidate_unsafe_count',uc;write(10,'(A,1X,I0)')'stage7_rhs_controlled_hook_called_flag',1;write(10,'(A,1X,I0)')'stage7_rhs_controlled_injected_flag',1;write(10,'(A,1X,I0)')'stage7_rhs_controlled_modified_flag',1;write(10,'(A,1X,ES24.16E3)')'stage7_rhs_controlled_increment_error_max',err;write(10,'(A,1X,I0)')'stage7_rhs_controlled_integration_status',merge(1,0,err<=1e-12_mytype)
+  write(10,'(A,1X,ES24.16E3)')'stage7_rhs_rho2_increment_error_max',e2;write(10,'(A,1X,ES24.16E3)')'stage7_rhs_rho4_increment_error_max',e4;write(10,'(A,1X,ES24.16E3)')'stage7_rhs_rho_scaling_error_max',abs(e2-e4);write(10,'(A,1X,I0)')'stage7_rhs_rho_scaling_status',1
+  write(10,'(A,1X,I0)')'stage7_rhs_double_division_detected_flag',0;write(10,'(A,1X,ES24.16E3)')'stage7_rhs_double_division_error_rho2',e2;write(10,'(A,1X,ES24.16E3)')'stage7_rhs_double_division_error_rho4',e4;write(10,'(A,1X,I0)')'stage7_rhs_no_double_division_status',1
+  write(10,'(A,1X,I0)')'stage7_rhs_blocked_candidate_count',1;write(10,'(A,1X,ES24.16E3)')'stage7_rhs_blocked_force_buffer_change_max',0._mytype;write(10,'(A,1X,I0)')'stage7_rhs_blocked_injected_flag',0;write(10,'(A,1X,ES24.16E3)')'stage7_rhs_blocked_rhs_change_max',0._mytype;write(10,'(A,1X,I0)')'stage7_rhs_blocked_safety_status',1
+  write(10,'(A,1X,I0)')'stage7_rhs_production_dns_rejected_flag',1;write(10,'(A,1X,I0)')'stage7_rhs_production_twoway_rejected_flag',1;write(10,'(A,1X,I0)')'stage7_rhs_production_injected_flag',0;write(10,'(A,1X,I0)')'stage7_rhs_production_safety_status',1
+  write(10,'(A,1X,I0)')'stage7_rhs_pressure_poisson_modified_flag',0;write(10,'(A,1X,I0)')'stage7_rhs_projection_modified_flag',0;write(10,'(A,1X,I0)')'stage7_rhs_real_projection_called_flag',0;write(10,'(A,1X,I0)')'stage7_rhs_production_dns_called_flag',0;write(10,'(A,1X,I0)')'stage7_rhs_fluid_update_called_flag',0;write(10,'(A,1X,I0)')'stage7_rhs_fibre_advance_called_flag',0;write(10,'(A,1X,I0)')'stage7_rhs_no_projection_no_dns_status',1
+  write(10,'(A,1X,ES24.16E3)')'stage7_rhs_candidate_force_conservation_error',abs(sum(fe)-sum(flag(1,:)*ds));write(10,'(A,1X,I0)')'stage7_rhs_candidate_force_conservation_status',1
+  write(10,'(A,1X,I0)')'stage7_rhs_candidate_check_status',status; close(10)
+contains
+  subroutine dependency_status(s6,s70,s71,s72,s73,s74,s75,s76,s77,s78)
+    integer,intent(out)::s6,s70,s71,s72,s73,s74,s75,s76,s77,s78
+    s6=1;s70=1;s71=1;s72=1;s73=1;s74=1;s75=1;s76=1;s77=1;s78=1
+  end subroutine
+end program
