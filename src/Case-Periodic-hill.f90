1a1856c97a3cc0113218e254296824174ad3b312
SQ <1954317499@qq.com>
2026-05-20 14:10:34 +0800

    Make Stage 9.1 IO compatibility layer self-contained

--- src/Case-Periodic-hill.f90
+++ src/Case-Periodic-hill.f90
@@ -136,7 +136,7 @@ contains
   subroutine init_hill (ux1,uy1,uz1,ep1,phi1)
 !############################################################################
 
-    USE decomp_2d_io
+    USE xcompact3d_decomp_io_compat
     USE variables
     USE param
     USE MPI
@@ -284,7 +284,7 @@ contains
   end subroutine postprocess_hill
   subroutine visu_hill_init(visu_initialised)
 
-    use decomp_2d_io, only : decomp_2d_register_variable
+    use xcompact3d_decomp_io_compat, only : decomp_2d_register_variable
     use visu, only : io_name, output2D
     
     implicit none
