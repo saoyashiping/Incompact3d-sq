5f1b7b4ac838acd14d688524e30cb4105683e5c0
SQ <1954317499@qq.com>
2026-05-22 16:09:40 +0800

    Update xcompact3d.f90

--- src/xcompact3d.f90
+++ src/xcompact3d.f90
@@ -64,7 +64,8 @@ program xcompact3d
   stage9_8_phase = stage9_8_get_phase()
   stage9_8_steps = merge(stage9_8_get_max_steps_after_restart(3), stage9_8_get_max_steps_before_restart(3), stage9_8_phase==1)
   stage9_8_sig_tol = stage9_8_get_signature_tol(1.0d-8)
-  call stage9_8_begin(stage9_8_reg, stage9_8_phase, stage9_8_steps, stage9_8_sig_tol)
+  ! Stage 9.8 begin is performed inside init_xcompact3d after reading the input file.
+  ! Reinitialising here would erase restart-read diagnostics captured during init.
   stage9_9_reg = stage9_9_requested()
   stage9_9_max_steps = stage9_9_get_max_steps(3)
   call stage9_9_get_tolerances(stage9_9_sig_tol, stage9_9_div_tol, stage9_9_massflux_tol)
@@ -296,8 +297,6 @@ subroutine init_xcompact3d()
 
   use mhd, only: mhd_init
   use particle,  only : particle_report,local_domain_size
-  use fibre_stage9_8_restart_io_regression, only : stage9_8_requested, stage9_8_get_phase, stage9_8_get_max_steps_before_restart, stage9_8_get_max_steps_after_restart, stage9_8_get_signature_tol, stage9_8_begin, stage9_8_record_restart_read_path, stage9_8_record_restart_file_status, stage9_8_record_signature
-
   implicit none
 
   integer :: ierr
@@ -410,13 +409,10 @@ subroutine init_xcompact3d()
   ! compute diffusion number of simulation
   call compute_cfldiff()
 
-  stage9_8_init_reg = stage9_8_requested()
-  stage9_8_init_phase = stage9_8_get_phase()
-  stage9_8_init_steps = merge(stage9_8_get_max_steps_after_restart(3), &
-       stage9_8_get_max_steps_before_restart(3), stage9_8_init_phase==1)
-  stage9_8_init_sig_tol = stage9_8_get_signature_tol(1.0d-8)
-  call stage9_8_begin(stage9_8_init_reg, stage9_8_init_phase, &
-       stage9_8_init_steps, stage9_8_init_sig_tol)
+  ! Stage 9.8 was already initialised immediately after parameter(InputFN),
+  ! before the possible production restart-read branch.  Do not reinitialise
+  ! it here, otherwise restart-read diagnostics/signatures recorded above can
+  ! be reset before the main program audits them.
 
   !####################################################################
   if (irestart==0) then
