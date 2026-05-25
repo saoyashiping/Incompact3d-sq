2c2c758d94aa09728de3e7552aab4e2c9f277d04
SQ <1954317499@qq.com>
2026-05-25 16:19:40 +0800

    Update fibre_stage10_noop_hook.f90

--- src/fibre_stage10_noop_hook.f90
+++ src/fibre_stage10_noop_hook.f90
@@ -16,6 +16,7 @@ module fibre_stage10_noop_hook
   integer, save :: no_rhs_injection_status = 1
   integer, save :: no_ibm_call_status = 1
   integer, save :: no_structure_advance_status = 1
+  integer, save :: field_modified_status = 0
 
   public :: stage10_hook_init
   public :: stage10_hook_pre_step
@@ -63,6 +64,7 @@ contains
 
   subroutine stage10_hook_finalize()
     finalize_status = 1
+    call stage10_write_main_noop_hook_dat_if_requested()
   end subroutine stage10_hook_finalize
 
   subroutine stage10_hook_get_status_values(requested, noop_mode, init_status, pre_step, pre_rhs, &
@@ -87,5 +89,62 @@ contains
     no_ibm_call = no_ibm_call_status
     no_structure_advance = no_structure_advance_status
   end subroutine stage10_hook_get_status_values
+
+  subroutine stage10_write_main_noop_hook_dat_if_requested()
+    character(len=64) :: value
+    integer :: env_status
+    integer :: unit_id
+    integer :: main_status
+
+    call get_environment_variable('X3D_STAGE10_3_MAIN_NOOP_HOOK', value=value, status=env_status)
+    if (env_status /= 0) return
+    if (trim(value) /= '1') return
+    if (.not. stage10_rank0_from_environment()) return
+
+    main_status = 0
+    if (requested_flag == 1 .and. noop_mode_status == 1 .and. &
+        initialized_status == 1 .and. pre_step_status == 1 .and. pre_rhs_status == 1 .and. &
+        post_projection_status == 1 .and. post_step_status == 1 .and. finalize_status == 1 .and. &
+        no_fibre_state_status == 1 .and. no_force_status == 1 .and. no_rhs_injection_status == 1 .and. &
+        no_ibm_call_status == 1 .and. no_structure_advance_status == 1 .and. field_modified_status == 0) then
+      main_status = 1
+    endif
+
+    open(newunit=unit_id, file='stage10_outputs/fibre_stage10_3_main_noop_hook.dat', status='replace', action='write', form='formatted')
+    write(unit_id,'(A,1X,I0)') 'stage10_3_requested_flag', requested_flag
+    write(unit_id,'(A,1X,I0)') 'stage10_3_noop_mode_status', noop_mode_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_hook_init_status', initialized_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_hook_pre_step_status', pre_step_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_hook_pre_rhs_status', pre_rhs_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_hook_post_projection_status', post_projection_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_hook_post_step_status', post_step_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_hook_finalize_status', finalize_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_no_fibre_state_status', no_fibre_state_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_no_force_status', no_force_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_no_rhs_injection_status', no_rhs_injection_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_no_ibm_call_status', no_ibm_call_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_no_structure_advance_status', no_structure_advance_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_field_modified_status', field_modified_status
+    write(unit_id,'(A,1X,I0)') 'stage10_3_main_noop_hook_status', main_status
+    close(unit_id)
+  end subroutine stage10_write_main_noop_hook_dat_if_requested
+
+  logical function stage10_rank0_from_environment()
+    character(len=64) :: value
+    integer :: env_status
+    integer :: rank_value
+    integer :: read_status
+
+    stage10_rank0_from_environment = .true.
+
+    call get_environment_variable('OMPI_COMM_WORLD_RANK', value=value, status=env_status)
+    if (env_status /= 0) call get_environment_variable('PMI_RANK', value=value, status=env_status)
+    if (env_status /= 0) call get_environment_variable('MPI_RANKID', value=value, status=env_status)
+    if (env_status /= 0) return
+
+    read(value, *, iostat=read_status) rank_value
+    if (read_status /= 0) return
+    stage10_rank0_from_environment = (rank_value == 0)
+  end function stage10_rank0_from_environment
 
 end module fibre_stage10_noop_hook
