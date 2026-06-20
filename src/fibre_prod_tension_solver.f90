e09801200b46ca45f433fbbc478ca6ed36e67faf
SQ <1954317499@qq.com>
2026-06-20 16:34:17 +0800

    Add files via upload

--- src/fibre_prod_tension_solver.f90
+++ src/fibre_prod_tension_solver.f90
@@ -12,7 +12,7 @@ module fibre_prod_tension_solver
 
 contains
 
-  pure real(dp) function fibre_prod_tension_segment_length_residual(x, ds, status) result(residual)
+  real(dp) function fibre_prod_tension_segment_length_residual(x, ds, status) result(residual)
     real(dp), intent(in) :: x(:, :)
     real(dp), intent(in) :: ds
     integer, intent(out) :: status
@@ -38,7 +38,7 @@ contains
     end if
   end function fibre_prod_tension_segment_length_residual
 
-  pure real(dp) function fibre_prod_tension_max_stretch_error(x, ds, status) result(max_error)
+  real(dp) function fibre_prod_tension_max_stretch_error(x, ds, status) result(max_error)
     real(dp), intent(in) :: x(:, :)
     real(dp), intent(in) :: ds
     integer, intent(out) :: status
